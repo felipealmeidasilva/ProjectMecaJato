3df5b0814192b496498f8828a0aa99826d51dccc
felipealmeidasilva <filipealmeidakukri@hotmail.com>
2023-09-25 07:39:43 -0300

    adicionando o banco de dados

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.nome
+    
+class Carro(models.Model):
+    carro = models.CharField(max_length=50)
+    placa = models.CharField(max_length=10)
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.carro   
--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Carro
 # Create your views here.
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == 'GET':
+        return render(request, 'clientes.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+        #cliente.save()
+
+        x = list(zip(carros, placas))
+        print(carros)
+        return HttpResponse('teste')
